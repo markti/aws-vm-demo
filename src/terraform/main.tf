1a4bc7e9faa2f076de220e74a4ccead948a8a912
markti <markti@outlook.com>
2023-09-27 06:14:01 -0400

    igw

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,6 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
