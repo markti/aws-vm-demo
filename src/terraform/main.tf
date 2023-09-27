30cfec8a031b3c5802a0ec2a81ee45e0fb892315
markti <markti@outlook.com>
2023-09-27 16:40:00 +0800

    output subnets

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -6,14 +6,24 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals {
+  public_subnets = cidrsubnets("10.0.0.0/16", 8, 8, 8)
+}
+
+output "public_subnets" {
+  value = local.public_subnets
+}
+
+/*
 resource "aws_subnet" "frontend" {
 
   count = length(data.aws_availability_zones.available.names)
 
   vpc_id            = aws_vpc.main.id
   availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = cidrsubnets("10.0.0.0/16", 8, 8, 8)[count.index]
+  cidr_block        = [count.index]
 }
+*/
 
 data "aws_ami" "frontend" {
   executable_users = ["self"]
