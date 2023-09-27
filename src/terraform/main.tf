2db5cee3a8cd0b908e0c6615d1d679479782341b
markti <markti@outlook.com>
2023-09-27 06:05:46 -0400

    add lb

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -50,3 +50,40 @@ resource "aws_instance" "frontend" {
   }
 
 }
+resource "aws_lb" "frontend" {
+  name                       = "${var.application_name}-${var.environment_name}"
+  internal                   = false
+  load_balancer_type         = "application"
+  subnets                    = aws_subnet.frontend.*.id
+  enable_deletion_protection = false # You can set this to true if you want to enable deletion protection
+  security_groups            = [aws_security_group.frontend_lb.id]
+}
+
+resource "aws_security_group" "frontend_lb" {
+  name        = "${var.application_name}-${var.environment_name}-frontend-lb-sg"
+  description = "Security group for the load balancer"
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "${var.application_name}-${var.environment_name}-frontend"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.frontend.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+resource "aws_lb_target_group_attachment" "frontend" {
+  count            = length(local.public_subnets)
+  target_group_arn = aws_lb_target_group.frontend.arn
+  target_id        = aws_instance.frontend[count.index].id
+}
--- src/terraform/terraform.tfvars
+++ src/terraform/terraform.tfvars
@@ -1,3 +1,5 @@
+application_name       = "fleet-portal"
+environment_name       = "dev"
 primary_region         = "us-west-2"
 frontend_image_name    = "frontend-2023.09.39"
 frontend_instance_type = "t2.small"
