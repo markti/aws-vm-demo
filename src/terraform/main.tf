abfe02fa6853c47b83e3d874d5baf9d95fb5a132
markti <markti@outlook.com>
2023-09-28 14:17:54 +0800

    update for http listener

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -86,7 +86,7 @@ resource "aws_security_group_rule" "https" {
   security_group_id = aws_security_group.frontend_lb.id
 }
 
-resource "aws_lb_target_group" "frontend" {
+resource "aws_lb_target_group" "frontend_http" {
   name                = "${var.application_name}-${var.environment_name}-frontend"
   port                = 80
   protocol            = "HTTP"
@@ -105,11 +105,11 @@ resource "aws_lb_listener" "frontend" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend.arn
+    target_group_arn = aws_lb_target_group.frontend_http.arn
   }
 }
-resource "aws_lb_target_group_attachment" "frontend" {
+resource "aws_lb_target_group_attachment" "frontend_http" {
   count            = length(local.public_subnets)
-  target_group_arn = aws_lb_target_group.frontend.arn
+  target_group_arn = aws_lb_target_group.frontend_http.arn
   target_id        = aws_instance.frontend[count.index].id
 }
