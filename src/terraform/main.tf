b75f4bf1b72e01e250aa9bb4986776480543db0d
markti <markti@outlook.com>
2023-09-28 14:10:38 +0800

    security group rules

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -69,6 +69,23 @@ resource "aws_security_group" "frontend_lb" {
   vpc_id      = aws_vpc.main.id
 }
 
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.frontend_lb.id
+}
+resource "aws_security_group_rule" "https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.main.cidr_block]
+  security_group_id = aws_security_group.frontend_lb.id
+}
+
 resource "aws_lb_target_group" "frontend" {
   name     = "${var.application_name}-${var.environment_name}-frontend"
   port     = 80
