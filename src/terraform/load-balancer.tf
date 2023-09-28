243893af942530511815003fb57556edc6067a15
markti <markti@outlook.com>
2023-09-28 14:38:18 +0800

    local attachments

--- src/terraform/load-balancer.tf
+++ src/terraform/load-balancer.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_lb" "frontend" {
   name                       = "${var.application_name}-${var.environment_name}"
   internal                   = false
@@ -8,26 +8,46 @@ resource "aws_lb" "frontend" {
   security_groups            = [aws_security_group.frontend_lb.id]
 }
 
-resource "aws_lb_target_group" "frontend_http" {
-  name     = "${var.application_name}-${var.environment_name}-frontend"
-  port     = 80
-  protocol = "HTTP"
+locals {
+  frontend_listeners = {
+    "HTTP"  = 80,
+    "HTTPS" = 443
+  }
+  frontend_attachments = [for k, v in local.frontend_listeners :
+    [for i in aws_instance.frontend : {
+      name        = k
+      port        = v
+      instance_id = i.id
+      }
+    ]
+  ]
+}
+
+resource "aws_lb_target_group" "frontend" {
+  for_each = local.frontend_listeners
+
+  name     = "${var.application_name}-${var.environment_name}-frontend-${each.key}"
+  port     = each.value
+  protocol = each.key
   vpc_id   = aws_vpc.main.id
 }
 
 resource "aws_lb_listener" "frontend" {
+  for_each = local.frontend_listeners
+
   load_balancer_arn = aws_lb.frontend.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = each.value
+  protocol          = each.key
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.frontend_http.arn
+    target_group_arn = aws_lb_target_group.frontend[each.key].arn
   }
 }
-resource "aws_lb_target_group_attachment" "frontend_http" {
-  count            = length(local.public_subnets)
-  target_group_arn = aws_lb_target_group.frontend_http.arn
+/*
+resource "aws_lb_target_group_attachment" "frontend" {
+  foreach
+  target_group_arn = aws_lb_target_group.frontend.arn
   target_id        = aws_instance.frontend[count.index].id
 }
 */
--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "public_subnets" {
   value = local.public_subnets
 }
+output "frontend_attachments" {
+  value = local.frontend_attachments
+}
