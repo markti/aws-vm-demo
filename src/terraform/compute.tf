79348b3dbd9b2bbe1e4b71bd98cad8281d0e5623
markti <markti@outlook.com>
2023-10-02 15:52:04 -0400

    add sg to nic

--- src/terraform/compute.tf
+++ src/terraform/compute.tf
@@ -20,13 +20,17 @@ resource "aws_network_interface" "frontend" {
   subnet_id = aws_subnet.frontend[count.index].id
 }
 
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.frontend_lb.id
+  network_interface_id = aws_instance.frontend.primary_network_interface_id
+}
+
 resource "aws_instance" "frontend" {
   count = length(local.public_subnets)
 
-  ami                    = data.aws_ami.frontend.id
-  instance_type          = var.frontend_instance_type
-  key_name               = aws_key_pair.frontend.key_name
-  vpc_security_group_ids = [aws_security_group.frontend_lb.id]
+  ami           = data.aws_ami.frontend.id
+  instance_type = var.frontend_instance_type
+  key_name      = aws_key_pair.frontend.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.frontend[count.index].id
