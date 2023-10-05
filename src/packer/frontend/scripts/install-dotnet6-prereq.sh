04f3b6b4de664f3632ee49c4e8ef4907203926cb
markti <markti@outlook.com>
2023-10-05 07:50:35 -0400

    get rid of declare

--- src/packer/frontend/scripts/install-dotnet6-prereq.sh
+++ src/packer/frontend/scripts/install-dotnet6-prereq.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-set -e
-
 # Get Ubuntu version
-declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
+repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 
 # Download Microsoft signing key and repository
 wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
