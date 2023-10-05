f77316a65bbec6b8b16af7de5aeab3b1b757a578
markti <markti@outlook.com>
2023-10-05 08:07:18 -0400

    echo repo version

--- src/packer/frontend/scripts/install-dotnet6-prereq.sh
+++ src/packer/frontend/scripts/install-dotnet6-prereq.sh
@@ -4,8 +4,12 @@
 repo_version_raw=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
 repo_version=$(echo $repo_version_raw | tr -d '\n' | tr -d '\r')
 
+echo "Repo Version: $repo_version"
+
 # Download Microsoft signing key and repository
-wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+url="https://packages.microsoft.com/config/ubuntu/${repo_version}/packages-microsoft-prod.deb"
+echo $url
+wget $url -O packages-microsoft-prod.deb
 
 # Install Microsoft signing key and repository
 dpkg -i packages-microsoft-prod.deb
