e797e860f5bf7a357f71c2661da6c41e4b2c51a6
markti <markti@outlook.com>
2023-10-04 21:26:51 -0400

    output private subnets

--- src/terraform/outputs.tf
+++ src/terraform/outputs.tf
@@ -1,6 +1,9 @@
 output "public_subnets" {
   value = local.public_subnets
 }
+output "private_subnets" {
+  value = local.private_subnets
+}
 /*
 output "frontend_attachments" {
   value = local.frontend_attachments
