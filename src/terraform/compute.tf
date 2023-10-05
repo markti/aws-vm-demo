24e96a553b57352f851aa5e0e9c5c3e5b0dfc425
markti <markti@outlook.com>
2023-10-05 15:03:09 -0400

    fix the sg on the ec2

--- src/terraform/compute.tf
+++ src/terraform/compute.tf
@@ -29,7 +29,7 @@ resource "aws_network_interface_sg_attachment" "sg_attachment" {
 
   for_each = aws_instance.frontend
 
-  security_group_id    = aws_security_group.frontend_lb.id
+  security_group_id    = aws_security_group.frontend.id
   network_interface_id = each.value.primary_network_interface_id
 
 }
