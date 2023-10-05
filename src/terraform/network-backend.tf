21130af823f487a793966af00855816e0d1ea9bd
markti <markti@outlook.com>
2023-10-04 21:35:31 -0400

    fix nat gw

--- src/terraform/network-backend.tf
+++ src/terraform/network-backend.tf
@@ -13,11 +13,14 @@ resource "aws_subnet" "backend" {
 }
 
 resource "aws_route_table" "backend" {
+
+  count = length(local.private_subnets)
+
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 }
 
@@ -26,7 +29,7 @@ resource "aws_route_table_association" "backend" {
   count = length(local.private_subnets)
 
   subnet_id      = aws_subnet.backend[count.index].id
-  route_table_id = aws_route_table.backend.id
+  route_table_id = aws_route_table.backend[count.index].id
 
 }
 
