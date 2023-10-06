ed97164cebf8965dd11a93087459e57327726c08
markti <markti@outlook.com>
2023-10-06 09:33:56 -0400

    add resource group

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -3,3 +3,23 @@ resource "aws_route53_zone" "main" {
   name = var.domain_name
 }
 */
+
+# setup a resource group
+resource "aws_resourcegroups_group" "main" {
+  name = "${var.application_name}-${var.environment_name}"
+
+  resource_query {
+    query = jsondecode({
+
+      TagFilters = [
+        {
+          Key    = "application",
+          Values = [var.application_name]
+        },
+        {
+          Key    = "environment",
+          Values = [var.environment_name]
+        }
+    ] })
+  }
+}
